b28f605ee46033feebecb51dedf161617eb32863
Erdal CEYLAN <erdalceylan25@gmail.com>
2025-05-23 18:38:57 +0300

    config fix

--- src/main/java/com/marketplaces/core/config/KafkaProducerConf.java
+++ src/main/java/com/marketplaces/core/config/KafkaProducerConf.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @EnableKafka
 public class KafkaProducerConf {
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaBootstrapServers;
 
     @Bean
